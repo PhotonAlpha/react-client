13b02e6ad91eebdd43b928a54f5c353dcdb9d2ce
tmpil9 <tmpil9>
2019-04-09 19:55:29 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const users = [
+    { username: 'Jerry', age: 21, gender: 'male' },
+    { username: 'Tomy', age: 22, gender: 'male' },
+    { username: 'Lily', age: 19, gender: 'female' },
+    { username: 'Lucy', age: 20, gender: 'female' }
+]
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+        count: 0
+      }
+    }
+    
+
+    handleClickOnTitle (world, e) {
+        console.log(this, world )
+        this.setState((prevState) => {
+            console.log('prevState', prevState)
+            return {count: 0};
+        });
+        this.setState((prevState) => {
+            return {count: prevState.count + 1};
+        });
+        this.setState((prevState) => {
+            return {count: prevState.count + 2};
+        });
+        console.log(this.get);
+    }
+
+    render() {
+        const elements = users.map((user, inx) => {
+            return (
+                <div key={inx} >
+                    <div>姓名：{user.username}</div>
+                    <div>年龄：{user.age}</div>
+                    <div>性别：{user.gender}</div>
+                    <hr />
+                </div>
+            )
+        })
+
+        return (
+            <div>{elements}</div>
+        );
+    }
 }
 
 export default App;
